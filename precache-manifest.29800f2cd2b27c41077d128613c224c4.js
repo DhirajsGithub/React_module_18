1a9a54142b594c0f81d18951b31a352cf2dd7526
Dhiraj Borse <borsedhiraj123@gmail.com>
2022-11-27 00:29:18 +0530

    Updates

---
+++ precache-manifest.29800f2cd2b27c41077d128613c224c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31545b0bb1c54b1f8a08113328c9e675",
+    "url": "/React_module_18/index.html"
+  },
+  {
+    "revision": "3eb4aafe49ba1de3c4f0",
+    "url": "/React_module_18/static/css/main.02adbb02.chunk.css"
+  },
+  {
+    "revision": "4808f55e6a7f6307ee5e",
+    "url": "/React_module_18/static/js/2.ff7dad85.chunk.js"
+  },
+  {
+    "revision": "3eb4aafe49ba1de3c4f0",
+    "url": "/React_module_18/static/js/main.fcc3b30a.chunk.js"
+  },
+  {
+    "revision": "57a89ec4616c3214ab8f",
+    "url": "/React_module_18/static/js/runtime~main.e050988e.js"
+  }
+]);
